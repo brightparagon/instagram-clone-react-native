36a85b6137d37f080469b3fdb968d7f0aeece010
노경모 <kyeongmo2@gmail.com>
2018-02-03 01:00:51 +0900

    modify images app: add TimeStampedModel(abstract), Image, Comment class

--- brightparagon/images/models.py
+++ brightparagon/images/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class TimeStampedModel(models.Model):
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+  # make it not linked to the database by django
+  class Meta:
+    abstract = True
+
+class Image(TimeStampedModel):
+  file = models.ImageField()
+  location = models.CharField(max_length=140)
+  caption = models.TextField()
+
+class Comment(TimeStampedModel):
+  message = models.TextField()
