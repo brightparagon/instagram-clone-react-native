2448e38426eb7c556723dcd7167550b87651070a
노경모 <kyeongmo2@gmail.com>
2018-02-04 00:15:56 +0900

    Add followers, following fields to User Model

--- brightparagon/users/models.py
+++ brightparagon/users/models.py
@@ -21,6 +21,8 @@ class User(AbstractUser):
     bio = models.TextField(null=True)
     phone = models.CharField(max_length=140, null=True)
     gender = models.CharField(max_length=80, choices=GENDER_CHOICES, null=True)
+    followers = models.ManyToManyField('self')
+    following = models.ManyToManyField('self')
 
     def __str__(self):
         return self.username
