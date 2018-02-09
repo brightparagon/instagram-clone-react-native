eddca25e9be89b1e127c9ad2edafa66393b2f9dc
노경모 <kyeongmo2@gmail.com>
2018-02-09 22:49:29 +0900

    Make urls for images

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     url(r'^accounts/', include('allauth.urls')),
 
     # Your stuff: custom urls includes go here
-
+    url(r'^images/', include('brightparagon.images.urls', namespace='images')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
