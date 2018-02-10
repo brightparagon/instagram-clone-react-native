458cab23d283340359f871ca76b79e14cc6f5f04
노경모 <kyeongmo2@gmail.com>
2018-02-10 11:34:59 +0900

    Add urls and views for comments and likes

--- brightparagon/images/urls.py
+++ brightparagon/images/urls.py
@@ -6,5 +6,15 @@ urlpatterns = [
     regex= r'^all/$',
     view=views.ListAllImages.as_view(),
     name='all_images'
+  ),
+  url(
+    regex= r'^comments/$',
+    view=views.ListAllComments.as_view(),
+    name='all_comments'
+  ),
+  url(
+    regex= r'^likes/$',
+    view=views.ListAllLikes.as_view(),
+    name='all_likes'
   )
 ]
