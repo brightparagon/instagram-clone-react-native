c50faa6d1b1db0a7819d6e6d6ca9824eae5373ab
노경모 <kyeongmo2@gmail.com>
2018-02-19 22:06:12 +0900

    User Feed: add views, urls

--- brightparagon/images/views.py
+++ brightparagon/images/views.py
@@ -2,34 +2,21 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from . import models, serializers
 
-class ListAllImages(APIView):
+class Feed(APIView):
 
   def get(self, request, format=None):
-    all_images = models.Image.objects.all()
-    serializer = serializers.ImageSerializer(all_images, many=True)
 
-    return Response(data=serializer.data)
+    user = request.user
 
-class ListAllComments(APIView):
+    following_users = user.following.all()
+    image_list = []
+    for following_user in following_users:
+      user_images = following_user.images.all()[:2]
+      for image in user_images:
+        image_list.append(image)
 
-  def get(self, request, format=None):
+    sorted_list = sorted(image_list, key=lambda image: image.created_at, reverse=True)
     
-    # get comments owned by the authenticated user
-    # user_id = request.user.id
-    # all_comments = models.Comment.objects.filter(creator=user_id)
+    serializer = serializers.ImageSerializer(sorted_list, many=True)
 
-    all_comments = models.Comment.objects.all()
-
-    serializer = serializers.CommentSerializer(all_comments, many=True)
- 
-    return Response(data=serializer.data) 
-
-class ListAllLikes(APIView):
-
-  def get(self, request, format=None):
-
-    all_likes = models.Like.objects.all()
-
-    serializer = serializers.LikeSerializer(all_likes, many=True)
-
-    return Response(data=serializer.data) 
+    return Response(serializer.data)
