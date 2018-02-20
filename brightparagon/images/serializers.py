6d352017f6ca2cdeaaface2a033941a2106c89ec
노경모 <kyeongmo2@gmail.com>
2018-02-21 00:26:19 +0900

    add profile_image field to User model & modify fields of Comment serializer: all -> id, message, creator

--- brightparagon/images/serializers.py
+++ brightparagon/images/serializers.py
@@ -1,11 +1,27 @@
 from rest_framework import serializers
 from . import models
+from brightparagon.users import models as user_models
+
+class FeedUserSerializer(serializers.ModelSerializer):
+
+  class Meta:
+    model = user_models.User
+    fields = (
+      'username',
+      'profile_image'
+    )
 
 class CommentSerializer(serializers.ModelSerializer):
 
+  creator = FeedUserSerializer()
+
   class Meta:
     model = models.Comment
-    fields = '__all__'
+    fields = (
+      'id',
+      'message',
+      'creator'
+    )
 
 class LikeSerializer(serializers.ModelSerializer):
 
@@ -16,7 +32,7 @@ class LikeSerializer(serializers.ModelSerializer):
 class ImageSerializer(serializers.ModelSerializer):
 
   comments = CommentSerializer(many=True)
-  likes = LikeSerializer(many=True)
+  creator = FeedUserSerializer()
 
   class Meta:
     model = models.Image
@@ -26,6 +42,7 @@ class ImageSerializer(serializers.ModelSerializer):
       'location',
       'caption',
       'comments',
-      'likes'
+      'like_count',
+      'creator'
     )
     
