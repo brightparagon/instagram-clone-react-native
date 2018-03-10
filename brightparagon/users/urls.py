cd83d0bdb3def58e92e45bac9f00423c52c56853
노경모 <kyeongmo2@gmail.com>
2018-03-10 12:12:43 +0900

    add an url for getting following users of a logged-in user

--- brightparagon/users/urls.py
+++ brightparagon/users/urls.py
@@ -27,4 +27,14 @@ urlpatterns = [
         view=views.UserFollowers.as_view(),
         name='user_followers'
     ),
+    url(
+        regex=r'^(?P<username>\w+)/followers/$',
+        view=views.UserFollowers.as_view(),
+        name='user_followers'
+    ),
+    url(
+        regex=r'^(?P<username>\w+)/following/$',
+        view=views.UserFollowing.as_view(),
+        name='user_following'
+    ),
 ]
