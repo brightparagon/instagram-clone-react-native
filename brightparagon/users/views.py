76800fb36942183a938c1f1411c4f1080c2246f8
노경모 <kyeongmo2@gmail.com>
2018-03-12 00:19:48 +0900

    add an url for searching users by user name

--- brightparagon/users/views.py
+++ brightparagon/users/views.py
@@ -83,3 +83,15 @@ class UserFollowing(APIView):
         serializer = serializers.ListUserSerializer(user_following, many=True)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
+class Search(APIView):
+
+    def get(self, request, format=None):
+        username = request.query_params.get('username', None)
+        users = models.User.objects.filter(username__istartswith=username)
+
+        if username is not None:
+            serializer = serializers.ListUserSerializer(users, many=True)
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
