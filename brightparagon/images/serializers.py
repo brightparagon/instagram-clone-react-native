13940cdec659de95a897e3b27e6ffb6c4abadf96
노경모 <kyeongmo2@gmail.com>
2018-03-17 01:11:27 +0900

    add an url for getting a single image withrelated information: creator, comments, like count

--- brightparagon/images/serializers.py
+++ brightparagon/images/serializers.py
@@ -14,55 +14,56 @@ class SmallImageSerializer(serializers.ModelSerializer):
 
 class CountImageSerializer(serializers.ModelSerializer):
 
-  class Meta:
-    model = models.Image
-    fields = (
-      'id',
-      'file',
-      'comment_count',
-      'like_count'
-    )
+    class Meta:
+        model = models.Image
+        fields = (
+          'id',
+          'file',
+          'comment_count',
+          'like_count'
+        )
 
 class FeedUserSerializer(serializers.ModelSerializer):
 
-  class Meta:
-    model = user_models.User
-    fields = (
-      'username',
-      'profile_image'
-    )
+    class Meta:
+        model = user_models.User
+        fields = (
+          'username',
+          'profile_image'
+        )
 
 class CommentSerializer(serializers.ModelSerializer):
 
-  creator = FeedUserSerializer(read_only=True)
+    creator = FeedUserSerializer(read_only=True)
 
-  class Meta:
-    model = models.Comment
-    fields = (
-      'id',
-      'message',
-      'creator'
-    )
+    class Meta:
+        model = models.Comment
+        fields = (
+          'id',
+          'message',
+          'creator'
+        )
 
 class LikeSerializer(serializers.ModelSerializer):
 
-  class Meta:
-    model = models.Like
-    fields = '__all__'
+    class Meta:
+        model = models.Like
+        fields = '__all__'
 
 class ImageSerializer(serializers.ModelSerializer):
 
-  comments = CommentSerializer(many=True)
-  creator = FeedUserSerializer()
+    comments = CommentSerializer(many=True)
+    creator = FeedUserSerializer()
 
-  class Meta:
-    model = models.Image
-    fields = (
-      'id',
-      'file',
-      'location',
-      'caption',
-      'comments',
-      'like_count',
-      'creator'
-    )
+    class Meta:
+        model = models.Image
+        fields = (
+            'id',
+            'file',
+            'location',
+            'caption',
+            'comments',
+            'like_count',
+            'creator',
+            'created_at'
+        )
--- brightparagon/images/urls.py
+++ brightparagon/images/urls.py
@@ -2,34 +2,39 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-  url(
-    regex= r'^$',
-    view=views.Feed.as_view(),
-    name='feed'
-  ),
-  url(
-    regex=r'^(?P<image_id>[0-9]+)/like/',
-    view=views.LikeImage.as_view(),
-    name='like_image'
-  ),
-  url(
-    regex=r'^(?P<image_id>[0-9]+)/unlike/',
-    view=views.UnLikeImage.as_view(),
-    name='unlike_image'
-  ),
-  url(
-    regex=r'^(?P<image_id>[0-9]+)/comments/(?P<comment_id>[0-9]+)/$',
-    view=views.ModerateComments.as_view(),
-    name='comment_image'
-  ),
-  url(
-    regex=r'comments/(?P<comment_id>[0-9]+)/$',
-    view=views.Comment.as_view(),
-    name='comment'
-  ),
-  url(
-    regex=r'search/$',
-    view=views.Search.as_view(),
-    name='search'
-  ),
+    url(
+        regex= r'^$',
+        view=views.Feed.as_view(),
+        name='feed'
+    ),
+    url(
+        regex= r'^(?P<image_id>[0-9]+)/$',
+        view=views.ImageDetail.as_view(),
+        name='image_detail'
+      ),
+    url(
+        regex=r'^(?P<image_id>[0-9]+)/like/',
+        view=views.LikeImage.as_view(),
+        name='like_image'
+    ),
+    url(
+        regex=r'^(?P<image_id>[0-9]+)/unlike/',
+        view=views.UnLikeImage.as_view(),
+        name='unlike_image'
+    ),
+    url(
+        regex=r'^(?P<image_id>[0-9]+)/comments/(?P<comment_id>[0-9]+)/$',
+        view=views.ModerateComments.as_view(),
+        name='comment_image'
+    ),
+    url(
+        regex=r'comments/(?P<comment_id>[0-9]+)/$',
+        view=views.Comment.as_view(),
+        name='comment'
+    ),
+    url(
+        regex=r'search/$',
+        view=views.Search.as_view(),
+        name='search'
+    ),
 ]
