622fd9379706a4c1830c766aee7e4bf35508c9ce
노경모 <kyeongmo2@gmail.com>
2018-03-17 15:35:51 +0900

    add pudeleteethond on Image Detail url: only author of an image can delete his/her image

--- brightparagon/images/views.py
+++ brightparagon/images/views.py
@@ -151,6 +151,13 @@ class ModerateComments(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 class ImageDetail(APIView):
+    def find_own_image(self, image_id, user):
+        try:
+            image = models.Image.objects.get(id=image_id, creator=user)
+            return image
+        except models.Image.DoesNotExist:
+            return None
+
     def get(self, request, image_id, format=None):
         user = request.user
 
@@ -162,12 +169,11 @@ class ImageDetail(APIView):
         serializer = serializers.ImageSerializer(image)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
-    def put(self, request, image_id, form=None):
+    def put(self, request, image_id, format=None):
         user = request.user
+        image = self.find_own_image(image_id, user)
 
-        try:
-            image = models.Image.objects.get(id=image_id, creator=user)
-        except models.Image.DoesNotExist:
+        if image is None:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
 
         serializer = serializers.InputImageSerializer(image, data=request.data, partial=True)
@@ -177,3 +183,13 @@ class ImageDetail(APIView):
             return Response(data=serializer.data, status=status.HTTP_204_NO_CONTENT)
         else:
             return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, image_id, format=None):
+        user = request.user
+        image = self.find_own_image(image_id, user)
+
+        if image is None:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+        image.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
