0569145ec34576722f5a6aba9196a663625bec83
노경모 <kyeongmo2@gmail.com>
2018-03-17 21:43:34 +0900

    install django rest auth and apply it in base.py and urls.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -49,8 +49,11 @@ THIRD_PARTY_APPS = [
     'allauth.account',  # registration
     'allauth.socialaccount',  # registration
     'rest_framework', # REST framework
+    'rest_framework.authtoken',
     'taggit', # tags for photos
     'taggit_serializer', # tag serializer
+    'rest_auth', # rest auth
+    'rest_auth.registration', # enable registration
 ]
 
 # Apps specific for this project go here.
@@ -255,8 +258,8 @@ AUTHENTICATION_BACKENDS = [
 
 # Some really nice defaults
 ACCOUNT_AUTHENTICATION_METHOD = 'username'
-ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_EMAIL_REQUIRED = False
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 ACCOUNT_ALLOW_REGISTRATION = env.bool('DJANGO_ACCOUNT_ALLOW_REGISTRATION', True)
 ACCOUNT_ADAPTER = 'brightparagon.users.adapters.AccountAdapter'
@@ -265,8 +268,6 @@ SOCIALACCOUNT_ADAPTER = 'brightparagon.users.adapters.SocialAccountAdapter'
 # Custom user app defaults
 # Select the correct user model
 AUTH_USER_MODEL = 'users.User'
-LOGIN_REDIRECT_URL = 'users:redirect'
-LOGIN_URL = 'account_login'
 
 # SLUGLIFIER
 AUTOSLUG_SLUGIFY_FUNCTION = 'slugify.slugify'
@@ -285,7 +286,8 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
     ),
 }
+
+REST_USE_JWT = True
+ACCOUNT_LOGOUT_ON_GET = True
