a2db991151d13d5b4a4ff929af320d585b7899e6
노경모 <kyeongmo2@gmail.com>
2018-03-27 00:14:47 +0900

    add users reducer: set initial state and export the reducer

---
+++ frontend/src/redux/modules/users.js
@@ -0,0 +1,12 @@
+const initialState = {
+  isLoggedIn: localStorage.getItem("jwt") || false
+}
+
+function reducer(state = initialState, action) {
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
+
+export default reducer;
