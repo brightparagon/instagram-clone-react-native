3551c8eb7e94564ed733d640763b451b9cbd429b
노경모 <kyeongmo2@gmail.com>
2018-03-27 23:30:21 +0900

    install redux, react-redux and create a redux store

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
-import styles from './App.scss';
+import styles from 'App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className={styles.App}>
-        <header className={styles.App__header}>
-          <h1 className={styles.App__title}>Welcome to React</h1>
-        </header>
-        <p className={styles.App__intro}>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
       </div>
     );
   }
