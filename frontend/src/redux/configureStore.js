72d0b4fc6a8890066decddda77596ca47057dac4
노경모 <kyeongmo2@gmail.com>
2018-03-28 22:07:42 +0900

    install redux-thunk & redux-logger and apply it

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,10 +1,20 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from 'redux-thunk';
 import users from "redux/modules/users";
+
+const NODE_ENV = process.env.NODE_ENV;
+
+const middlewares = [thunk];
+
+if (NODE_ENV === "development") {
+  const { logger } = require("redux-logger");
+  middlewares.push(logger);
+}
 
 const reducer = combineReducers({
   users
 });
 
-const store = initialState => createStore(reducer);
+const store = initialState => createStore(reducer, applyMiddleware(...middlewares));
 
 export default store();
