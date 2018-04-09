fb814246273d1a2c4451db33476b0bbb14d5d7ed
노경모 <kyeongmo2@gmail.com>
2018-04-09 21:31:34 +0900

    make Footer component and style it & add dashes options to webpack sass loader

--- frontend/config/webpack.config.dev.js
+++ frontend/config/webpack.config.dev.js
@@ -166,6 +166,7 @@ module.exports = {
                   importLoaders: 1,
                   modules: true,
                   localIdentName: '[path][name]__[local]--[hash:base64:5]',
+                  camelCase: "dashes",
                 },
               },
               {
--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
-import styles from './App.scss';
+import React, { Component } from "react";
+import styles from "./styles.scss";
+import Footer from "components/Footer";
 
 class App extends Component {
   render() {
     return (
       <div className={styles.App}>
-        wow
+        <Footer />
       </div>
     );
   }
