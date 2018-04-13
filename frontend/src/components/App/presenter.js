cd83b193608079cd47288ed49f87495a28174c30
노경모 <kyeongmo2@gmail.com>
2018-04-13 23:15:45 +0900

    modify presenter for App component

--- frontend/src/components/App/presenter.js
+++ frontend/src/components/App/presenter.js
@@ -1,15 +1,25 @@
-import React, { Component } from "react";
-import "./styles.scss";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import styles from "./styles.scss";
 import Footer from "components/Footer";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Footer />
-      </div>
-    );
-  }
-}
+const App = props => [
+  props.isLoggedIn ? <PrivateRoutes key={2} /> : <PublicRoutes key={2} />,
+  <Footer key={3} />
+];
+
+const PrivateRoutes = props => (
+  <Switch>
+    <Route key="1" exact path="/" render={() => "feed"} />,
+    <Route key="2" path="explore" render={() => "explore"} />
+  </Switch>
+);
+
+const PublicRoutes = props => (
+  <Switch>
+    <Route exact path="/" render={() => "login"} />,
+    <Route path="/recover" render={() => "recover password"} />
+  </Switch>
+);
 
 export default App;
