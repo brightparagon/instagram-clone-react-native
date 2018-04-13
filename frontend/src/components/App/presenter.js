7c6e84b97dded552ff3057a3b702e9a7f77222af
노경모 <kyeongmo2@gmail.com>
2018-04-13 23:38:23 +0900

    add container, presenter, css for Auth component

--- frontend/src/components/App/presenter.js
+++ frontend/src/components/App/presenter.js
@@ -1,12 +1,18 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Route, Switch } from "react-router-dom";
-import styles from "./styles.scss";
+import "./styles.scss";
 import Footer from "components/Footer";
+import Auth from "components/Auth";
 
 const App = props => [
   props.isLoggedIn ? <PrivateRoutes key={2} /> : <PublicRoutes key={2} />,
   <Footer key={3} />
 ];
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired
+};
 
 const PrivateRoutes = props => (
   <Switch>
@@ -17,7 +23,7 @@ const PrivateRoutes = props => (
 
 const PublicRoutes = props => (
   <Switch>
-    <Route exact path="/" render={() => "login"} />,
+    <Route exact path="/" component={Auth} />,
     <Route path="/recover" render={() => "recover password"} />
   </Switch>
 );
---
+++ frontend/src/components/Auth/presenter.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Auth = (props, context) => {
+  return <h1>???</h1>
+};
+
+export default Auth;
