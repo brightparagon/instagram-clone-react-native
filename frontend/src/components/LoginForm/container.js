6d5f964f5307d3cd406555a742fb2a740d9ea953
노경모 <kyeongmo2@gmail.com>
2018-05-05 00:16:45 +0900

    apply facebooklogin to LoginForm component, connect actions to reducer

--- frontend/src/components/LoginForm/container.js
+++ frontend/src/components/LoginForm/container.js
@@ -1,10 +1,15 @@
 import React from "react";
+import PropTypes from "prop-types";
 import LoginForm from "./presenter";
 
 class Container extends React.Component {
   state = {
     username: "",
     password: ""
+  };
+
+  static propTypes = {
+    facebookLogin: PropTypes.func.isRequired
   };
 
   _handleInputChange = event => {
@@ -20,7 +25,8 @@ class Container extends React.Component {
   }
 
   _handleFacebookLogin = response => {
-    console.log(response)
+    const { facebookLogin } = this.props;
+    facebookLogin(response.accessToken);
   }
 
   render() {
--- frontend/src/redux/modules/user.js
+++ frontend/src/redux/modules/user.js
@@ -2,11 +2,34 @@ const initialState = {
   isLoggedIn: localStorage.getItem("jwt") || false
 }
 
+function facebookLogin(access_token) {
+  return dispatch => {
+    fetch("/users/login/facebook/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        access_token
+      })
+    })
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(error => console.error(error));
+  }
+}
+
 function reducer(state = initialState, action) {
   switch(action.type) {
     default:
       return state;
   }
 }
+
+const actionCreators = {
+  facebookLogin
+};
+
+export { actionCreators };
 
 export default reducer;
