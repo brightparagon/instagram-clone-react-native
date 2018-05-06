26e16498f4372c0a6a1aa2a6f2a413d797c8e267
노경모 <kyeongmo2@gmail.com>
2018-05-06 23:49:43 +0900

    add action creators and reducer functions in user reducer

--- frontend/src/redux/modules/user.js
+++ frontend/src/redux/modules/user.js
@@ -1,7 +1,19 @@
 const initialState = {
-  isLoggedIn: localStorage.getItem("jwt") || false
+  isLoggedIn: localStorage.getItem("jwt") ? true : false
 }
 
+// actions
+const SAVE_TOKEN = "SAVE_TOKEN";
+
+// action creators
+function saveToken(token) {
+  return {
+    type: SAVE_TOKEN,
+    token
+  }
+}
+
+// API actions
 function facebookLogin(access_token) {
   return dispatch => {
     fetch("/users/login/facebook/", {
@@ -14,16 +26,34 @@ function facebookLogin(access_token) {
       })
     })
     .then(response => response.json())
-    .then(json => console.log(json))
+    .then(json => {
+      if(json.token) {
+        localStorage.setItem("jwt", json.token);
+        dispatch(saveToken(json.token));
+      }
+    })
     .catch(error => console.error(error));
   }
 }
 
+// reducer
 function reducer(state = initialState, action) {
   switch(action.type) {
+    case SAVE_TOKEN:
+      return applySetToken(state, action);
     default:
       return state;
   }
+}
+
+// reducer functions
+function applySetToken(state, action) {
+  const { token } = action;
+  return {
+    ...state,
+    isLoggedIn: true,
+    token
+  };
 }
 
 const actionCreators = {
