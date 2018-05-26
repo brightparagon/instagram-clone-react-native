e91496b282cf9f14a464966ec529b2b4d111afff
노경모 <kyeongmo2@gmail.com>
2018-05-26 16:49:20 +0900

    Install react-textarea-autosize & Add styles to CommentBox, FeedPhoto components

--- frontend/src/components/CommentBox/presenter.js
+++ frontend/src/components/CommentBox/presenter.js
@@ -1,10 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Textarea from "react-textarea-autosize";
 import styles from "./styles.scss";
 
 const CommentBox = (props, context) => (
   <form>
-    <textarea placeholder={context.t("Add a comment...")} />
+    <Textarea placeholder={context.t("Add a comment...")} />
   </form>
 );
 
