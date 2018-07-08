b15dae371340515b54354947ac4814e6638cb69b
노경모 <kyeongmo2@gmail.com>
2018-07-08 22:18:00 +0900

    Fix mistyping in index.js of PhotoActions component

--- frontend/src/components/PhotoActions/index.js
+++ frontend/src/components/PhotoActions/index.js
@@ -1,6 +1,6 @@
 import { connect } from "react-redux";
 import Container from "./container";
-import { actionCreators as photoActions } from "redux/modules/photos";
+import { actionCreators as photoActions } from "redux/modules/photo";
 
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
