21999c07c6580ce2febfc3064806d70632c50fe5
노경모 <kyeongmo2@gmail.com>
2018-07-09 22:38:19 +0900

    Rearrange codes UserList component

--- frontend/src/components/UserList/container.js
+++ frontend/src/components/UserList/container.js
@@ -5,14 +5,14 @@ class Container extends Component {
   state = {
     loading: true
   };
-  
+
   componentDidMount() {
     const { userList } = this.props;
     if (userList) {
       this.setState({ loading: false });
     }
   }
-
+  
   componentWillReceiveProps(nextProps) {
     if (nextProps.userList) {
       this.setState({
