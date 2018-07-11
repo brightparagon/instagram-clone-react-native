1e46375a0fc3b6dbf6e70ae946fef0c7dd002e19
노경모 <kyeongmo2@gmail.com>
2018-07-11 23:55:10 +0900

    Preload assets: show a loading image until assets are ready

--- instagramapp/App.js
+++ instagramapp/App.js
@@ -1,8 +1,48 @@
 import React from 'react';
+import { AppLoading, Asset, Font } from 'expo';
+import { Ionicons, MaterialIcons } from '@expo/vector-icons';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
+class App extends React.Component {
+  state = {
+    isLoadingComplete: false
+  };
+
+  _loadAssetsAsync = async () => {
+    return Promise.all([
+      Asset.loadAsync([
+        require('./assets/images/swiss.jpeg'),
+        require('./assets/images/jungfrau.jpeg')
+      ]),
+      Font.loadAsync({
+        ...Ionicons.font,
+        ...MaterialIcons.font
+      })
+    ]);
+  };
+
+  _handleFinishLoading = (error) => {
+    console.log(error)
+  };
+
+  _handleLoadingError = async () => {
+    this.setState({
+      isLoadingComplete: true
+    });
+  };
+
   render() {
+    const { isLoadingComplete } = this.state;
+    if (!isLoadingComplete) {
+      return (
+        <AppLoading
+          startAsync={this._loadAssetsAsync}
+          onError={this._handleLoadingError}
+          onFinish={this._handleFinishLoading}
+        />
+      );
+    }
+
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
@@ -19,3 +59,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
