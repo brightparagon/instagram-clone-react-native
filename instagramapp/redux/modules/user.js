b3cae94d1dae047d5edec76a34e3b4e9162cb552
노경모 <kyeongmo2@gmail.com>
2018-07-16 23:48:02 +0900

    Add redux store with redux-persist

---
+++ instagramapp/redux/modules/user.js
@@ -0,0 +1,12 @@
+const initialState = {
+  isLoggedIn: false,
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default reducer;
